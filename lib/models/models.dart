82c084a41097eb8eee4c1d621d416131c3c15c03
Kshitiz <kshitizagarwa2405@gmail.com>
2023-01-28 18:42:01 +0530

    Trial Commit

---
+++ lib/models/models.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Model extends StatelessWidget {
+  const Model({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
