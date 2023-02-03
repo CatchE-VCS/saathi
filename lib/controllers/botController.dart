9bad439a0edcf9a9b42d02b8ef1bfcab58814a06
Kshitiz <kshitizagarwa2405@gmail.com>
2023-02-03 15:25:54 +0530

    Chat Bot

---
+++ lib/controllers/botController.dart
@@ -0,0 +1,97 @@
+import 'package:dialog_flowtter/dialog_flowtter.dart';
+import 'package:flutter/material.dart';
+import 'package:saathi/utils/messages.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late DialogFlowtter dialogFlowtter;
+  final TextEditingController _controller = TextEditingController();
+
+  List<Map<String, dynamic>> messages = [];
+
+  @override
+  void initState() {
+    DialogFlowtter.fromFile().then((instance) => dialogFlowtter = instance);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFD495),
+      appBar: AppBar(
+        title: Text(
+          'Your Dost',
+          style:
+              TextStyle(color: Color(0xFFFFD495), fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Color(0xFFFAAB78),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(child: MessagesScreen(messages: messages)),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+              color: Color(0xFFFAAB78),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                    controller: _controller,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: "Type your message here",
+                      filled: true,
+                      fillColor: Colors.black,
+                      // labelStyle:
+                      //     TextStyle(fontFamily: GoogleFonts.poppins().fontFamily),
+                    ),
+                  )),
+                  IconButton(
+                      onPressed: () {
+                        sendMessage(_controller.text);
+                        _controller.clear();
+                      },
+                      icon: Icon(
+                        Icons.send,
+                        color: Color(0xFFFFD495),
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  sendMessage(String text) async {
+    print(text);
+    if (text.isEmpty) {
+      print('Message is empty');
+    } else {
+      setState(() {
+        addMessage(Message(text: DialogText(text: [text])), true);
+      });
+
+      DetectIntentResponse response = await dialogFlowtter.detectIntent(
+          queryInput: QueryInput(text: TextInput(text: text)));
+      if (response.message == null) return;
+      setState(() {
+        addMessage(response.message!);
+      });
+    }
+  }
+
+  addMessage(Message message, [bool isUserMessage = false]) {
+    print(message);
+    messages.add({'message': message, 'isUserMessage': isUserMessage});
+  }
+}
