524bc81a2cbf0bb45ce10b4721e287366fa90ccc
AKGEC <utkarsh2110024@akgec.ac.in>
2023-02-04 06:08:17 +0530

    v1.0.6

--- lib/controllers/postController.dart
+++ lib/controllers/postController.dart
@@ -4,27 +4,28 @@ import 'package:http/http.dart' as http;
 import 'package:saathi/models/user_post_model.dart';
 
 class Post {
-  Future<String> addPost(name, profileUrl, headline, tags, description,
-      isVideoPost, comments, video, isOnline, image, likes) async {
+  Future<String> addPost(name, profileUrl, description, image) async {
     var response = await http.post(
-        Uri.parse("http://malicious-place-production.up.railway.app/addPost"),
+        Uri.parse("https://malicious-place-production.up.railway.app/addPost"),
         body: json.encode({
           "name": name,
           "profileUrl": profileUrl,
-          "headline": headline,
-          "tags": tags,
+          "headline": "Author",
+          "tags": "",
           "description": description,
-          "isVideoPost": isVideoPost,
-          "comments": comments,
-          "video": video,
-          "isOnline": isOnline,
+          "isVideoPost": false,
+          "comments": "0",
+          "video": "",
+          "isOnline": true,
           "image": image,
-          "likes": likes
+          "likes": "0"
         }));
     if (response.statusCode == 200) {
       String data = response.body;
+      print(data);
       return data;
     } else {
+      print("object");
       return response.body;
     }
   }
--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -1,89 +1,123 @@
 import 'package:flutter/material.dart';
 import 'package:saathi/controllers/postController.dart';
 import 'package:saathi/screens/home/home_screen.dart';
+import 'package:saathi/utils/const.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:saathi/screens/home/home_screen.dart';
 
 class PostWidget extends StatelessWidget {
-  final user = 
-  final textController  = TextEditingController();
-  final _pc ;
+  final userName = firebaseAuth.currentUser?.displayName;
+  final userProfUrl = firebaseAuth.currentUser?.photoURL;
+  final textController = TextEditingController();
+  final _pc;
   PostWidget(this._pc, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffffebec),
-      appBar: PreferredSize(
-        preferredSize: Size.fromRadius(35),
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: AppBar(
-              backgroundColor: const Color(0xffE5D0ED),
-              leading:  IconButton(icon: const Icon(Icons.cancel_outlined), onPressed: () {
-                _pc.close();
-                Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
-              },),
-              actions: const [
-                IconButton(onPressed: null, icon: Icon(Icons.access_time_rounded)),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: ElevatedButton(onPressed: ()=> Post().addPost(textController), child: Text("Post", style: TextStyle(color: Colors.white),)),
+        backgroundColor: const Color(0xffffebec),
+        appBar: PreferredSize(
+          preferredSize: Size.fromRadius(35),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: AppBar(
+                backgroundColor: const Color(0xffE5D0ED),
+                leading: IconButton(
+                  icon: const Icon(Icons.cancel_outlined),
+                  onPressed: () {
+                    _pc.close();
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  },
+                ),
+                actions: [
+                  IconButton(
+                      onPressed: null, icon: Icon(Icons.access_time_rounded)),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                        onPressed: () =>
+                            Post().addPost(userName, userProfUrl, textController.text,""),
+                        child: Text(
+                          "Post",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(9.0),
+            child: Column(
+              children: [
+                TextField(
+                  maxLength: 700,
+                  maxLines: 29,
+                  decoration: InputDecoration(
+                    hintText: "What do you want to talk about?",
+                  ),
+                  controller: textController,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(25),
+                  child: Container(
+                      color: Color(0xffE5D0ED),
+                      margin: EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: const [
+                              IconButton(
+                                onPressed: null,
+                                icon: Icon(
+                                  Icons.camera_alt,
+                                  color: Color(0xff710193),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: null,
+                                icon: Icon(Icons.video_call),
+                                color: Color(0xff710193),
+                              ),
+                              IconButton(
+                                onPressed: null,
+                                icon: Icon(Icons.photo_library_outlined),
+                                color: Color(0xff710193),
+                              ),
+                              IconButton(
+                                  onPressed: null,
+                                  icon: Text(
+                                    "...",
+                                    style: TextStyle(
+                                      color: Color(0xff710193),
+                                    ),
+                                  )),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Row(
+                              children: const [
+                                IconButton(
+                                    onPressed: null,
+                                    icon: Icon(Icons.message_outlined),
+                                    color: Color(0xff710193)),
+                                Text("Anyone"),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )),
                 ),
               ],
             ),
           ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(9.0),
-          child: Column(
-            children: [
-              TextField(
-                maxLength: 700,
-                maxLines: 29,
-                decoration: InputDecoration(
-                  hintText: "What do you want to talk about?",
-                ),
-                controller: textController,
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(25),
-                child: Container(
-                  color: Color(0xffE5D0ED),
-                    margin: EdgeInsets.all(10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: const [
-                            IconButton(onPressed: null, icon: Icon(Icons.camera_alt, color: Color(0xff710193),),),
-                            IconButton(onPressed: null, icon: Icon(Icons.video_call), color: Color(0xff710193),),
-                            IconButton(onPressed: null, icon: Icon(Icons.photo_library_outlined), color: Color(0xff710193),),
-                            IconButton(onPressed: null, icon: Text("...", style: TextStyle(color: Color(0xff710193),),)),
-                          ],
-                        ),
-
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: Row(
-                            children: const [
-                              IconButton(onPressed: null, icon: Icon(Icons.message_outlined) , color: Color(0xff710193)),
-                              Text("Anyone"),
-                            ],
-                          ),
-                        ),
-                      ],
-                    )
-                ),
-              ),
-            ],
-          ),
-        ),
-      )
-    );
+        ));
   }
 }
