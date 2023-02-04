d63fc6a08d73f0c9ffe3ab1a49f5d1b4bc8d2402
hammad khan <hammad2000khan6528@gmail.com>
2023-02-04 06:12:18 +0530

    profile

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -7,6 +7,7 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: BackButton(color: Colors.indigo),
         elevation: 0,
         backgroundColor: Colors.white,
         actions: const [
@@ -16,79 +17,82 @@ class ProfilePage extends StatelessWidget {
       body: Column(
         children: [
           ClipRRect(
-            borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
-            child: Column(
+            borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20)),
+            child: Column(children: [
+              const Text(
+                "My Profile",
+                style: TextStyle(fontSize: 30),
+              ),
+              Column(
                 children: [
-                  const Text("My Profile", style: TextStyle(fontSize: 30),),
-                  Column(
+                  CircleAvatar(
+                    child: Image.asset("assets/profile1.jpg"),
+                    radius: 25,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Guildhall School of music and drama",
+                      style: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(3.0),
+                    child: Text(
+                      "London, UK",
+                      style: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      CircleAvatar(child: Image.asset("assets/profile1.jpg"), radius: 25,),
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Guildhall School of music and drama", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [Text("Photos"), Text("456")],
+                        ),
                       ),
-                      const Padding(
-                        padding: EdgeInsets.all(3.0),
-                        child: Text("London, UK", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [Text("Photos"), Text("456")],
+                        ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text("Photos"),
-                                Text("456")
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text("Photos"),
-                                Text("456")
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text("Photos"),
-                                Text("456")
-                              ],
-                            ),
-                          )
-                        ],
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [Text("Photos"), Text("456")],
+                        ),
                       )
                     ],
                   )
-                ]
-              ),
+                ],
+              )
+            ]),
           ),
           Container(
             height: 400,
-            width: 200,
+            width: 400,
             child: GridView.count(
                 mainAxisSpacing: 1,
-              crossAxisSpacing: 1,
-              childAspectRatio: 1,
-              primary: true,
-              shrinkWrap: true,
-              crossAxisCount: 2,
-              children: [
-                Image.asset("assets/profile4.jpg"),
-                Image.asset("assets/profile2.jpg"),
-                Image.asset("assets/profile3.jpg"),
-                Image.asset("assets/profile4.jpg"),
-
-              ]
-          ),
+                crossAxisSpacing: 1,
+                childAspectRatio: 1,
+                primary: true,
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                children: [
+                  Image.asset("assets/profile4.jpg"),
+                  Image.asset("assets/profile2.jpg"),
+                  Image.asset("assets/profile3.jpg"),
+                  Image.asset("assets/profile4.jpg"),
+                ]),
           )
         ],
       ),
